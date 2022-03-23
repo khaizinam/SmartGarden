769c9389975256aeabc14b095156b1c2363be6d3
Leonadon <84978080+khaizinam@users.noreply.github.com>
2022-03-23 08:03:11 +0700

    a

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,3 +1,113 @@
+const LOGIN_PAGE = `<div class="form-log">
+<img src="./favicon.png" alt="icon" width="100px"> <br>
+<label for="input-username">User name :</label>
+<br>
+<input onkeydown="app.checkform()" type="text" id="input-username">
+<br>
+<span id="err-usr-name" class="err"></span>
+<br>
+<label for="input-pw">Pass word :</label> <br>
+<input onkeydown="app.checkform()" type="password" id="input-pw"><br>
+<span id="err-pw" class="err"></span>
+<br>
+<button onclick="app.loginClick()" class="btn-sign-in">Sign in</button>
+<br>
+<button onclick="" class="btn-fogot">Fogot password !</button>
+</div>`,
+    PAGE_DETAIL = ` <div id="body">
+<div class="component-head container">
+<div style="height: 160px;"></div>
+<img style="position:absolute;top:-70px;width: 100%;" src="./asset/img/bg-1.jpg" alt="bg">
+<div style="position: absolute;width: 100%" class="bar-1">
+<div>
+<span id="user-name">User name</span>
+<span id="user-id">#1075</span>
+</div>
+</div>
+<div class="wrapper-user">
+<img src="./asset/img/user-1.jpg" alt="bg">
+</div>
+</div>
+<div style="display: flex;justify-content: space-between;">
+<button onclick="app.ChangeMainPage()" id="btn-mod" class="btn-mod">Trở về</button>
+<button onclick="app.setting()" id="btn-mod" class="btn-mod">Cài đặt</button>
+</div>
+<div id="conponent-1">
+<div id="conponent-1-left">
+<div class="box-1">
+<span>Temperature</span>
+<br>
+<span id="temperature">16.6 ℃</span>
+</div>
+<div class="box-1">
+<span>soil Moisture</span>
+<br>
+<span id="soil-moisture">137.5 %</span>
+</div>
+<div class="box-1">
+<span>Solar Input</span>
+<br>
+<span id="solar-input">16.6 V</span>
+</div>
+</div>
+<div id="conponent-1-right">
+<div class="box-1">
+<span>Humidity</span>
+<br>
+<span id="hunidity">90.7 %</span>
+</div>
+<div class="box-1">
+<span>Battery Voltage</span>
+<br>
+<span id="battery-voltage">90.7 %</span>
+</div>
+<div class="box-1">
+<span>Water</span>
+<br>
+<span id="water">90.7 %</span>
+</div>
+</div>
+</div>
+<div style="margin-top:50px">
+<div class="list-mod">
+<ul class="list-mod-2">
+<li>
+<span>Auto Water </span>
+<button data-="AUTO_WATER" onclick="app.clickMod(this)" id="btn-auto-water" class="btn-mod">ON</button>
+</li>
+</ul>
+</div>
+</div>
+</div>`,
+    MAIN_PAGE = `<div id="body">
+<div class="component-head container">
+<div style="height: 160px;"></div>
+<img style="position:absolute;top:-70px;width: 100%;" src="./asset/img/bg-1.jpg" alt="bg">
+<div style="position: absolute;width: 100%" class="bar-1">
+<div>
+<span id="user-name">User name</span>
+<span id="user-id">#1075</span>
+</div>
+</div>
+<div class="wrapper-user">
+<img src="./asset/img/user-1.jpg" alt="bg">
+</div>
+</div>
+<div style="margin-top:50px">
+<button onclick="app.logout()" class="btn-mod">Đăng xuất</button>
+<div class="list-mod">
+<h2>Tìm kiếm :</h2>
+<input id="q-micro" type="text">
+<button class="btn-mod">Tìm tất cả</button>
+<ul class="list-mod-2">
+<li>
+<span>Microbit 1: </span>
+<button data-="125" onclick="app.pagedetail(this)" class="btn-mod">SELECT</button>
+</li>
+</ul>
+</div>
+</div>
+</div>`;
 class App {
     constructor() {
         this.user = {
@@ -12,32 +122,21 @@ class App {
                 "id": "btn-auto-water"
             }
         };
-
-        this.controll();
+        this.runfunction = "";
     }
-    controll() {
-        if (this.user.TOKEN == "none") {
-            this.loginpage();
-        } else this.boot();
+    Networkmainpage() {
+        console.log("main page");
     }
-    loginpage() {
-        document.getElementById("wrapper-all").innerHTML =
-            `<div class="form-log">
-        <img src="./favicon.png" alt="icon" width="100px"> <br>
-        <label for="input-username">User name :</label>
-        <br>
-        <input onkeydown="app.checkform()" type="text" id="input-username">
-        <br>
-        <span id="err-usr-name" class="err"></span>
-        <br>
-        <label for="input-pw">Pass word :</label> <br>
-        <input onkeydown="app.checkform()" type="password" id="input-pw"><br>
-        <span id="err-pw" class="err"></span>
-        <br>
-        <button onclick="app.loginClick()" class="btn-sign-in">Sign in</button>
-        <br>
-        <button onclick="" class="btn-fogot">Fogot password !</button>
-    </div>`;
+    Networkpagedetail() {
+        console.log("page detail");
+    }
+    showLoginpage() {
+        document.getElementById("wrapper-all").innerHTML = LOGIN_PAGE;
+    }
+    ChangeMainPage() {
+        this.showMainPage();
+        this.printname();
+        this.runfunction = "main-page";
     }
     checkform() {
         let usrname = document.getElementById("input-username");
@@ -88,87 +187,26 @@ class App {
                                 name: usrname.value,
                                 id: res.id
                             }
+                            setCookie("token", app.user.TOKEN, "30")
                             console.log(app.user.TOKEN + "\n" + app.user.id);
-                            app.boot();
+                            app.ChangeMainPage();
                         }
                     }
                 });
         }
     }
-    boot() {
-        this.show();
-        this.printname();
-        this.UpdateMod();
-    }
-    show() {
-        document.getElementById("wrapper-all").innerHTML =
-            `<div id="body">
-                    <div class="component-head container">
-                        <div style="height: 160px;"></div>
-                        <div class="bar-1">
-                            <div>
-                                <span id="user-name">User name</span>
-                                <span id="user-id">#1075</span>
-                            </div>
-                            <div>
-                                <span>User name</span>
-                                <span>#1075</span>
-                            </div>
-                        </div>
-                        <div class="wrapper-user">
-                            <img src="./asset/img/user-1.jpg" alt="bg">
-                        </div>
-                    </div>
-                    <div id="conponent-1">
-                        <div id="conponent-1-left">
-                            <div class="box-1">
-                                <span>Temperature</span>
-                                <br>
-                                <span id="temperature">16.6 ℃</span>
-                            </div>
-                            <div class="box-1">
-                                <span>soil Moisture</span>
-                                <br>
-                                <span id="soil-moisture">137.5 %</span>
-                            </div>
-                            <div class="box-1">
-                                <span>Solar Input</span>
-                                <br>
-                                <span id="solar-input">16.6 V</span>
-                            </div>
-                        </div>
-                        <div id="conponent-1-right">
-                            <div class="box-1">
-                                <span>Humidity</span>
-                                <br>
-                                <span id="hunidity">90.7 %</span>
-                            </div>
-                            <div class="box-1">
-                                <span>Battery Voltage</span>
-                                <br>
-                                <span id="battery-voltage">90.7 %</span>
-                            </div>
-                            <div class="box-1">
-                                <span>Water</span>
-                                <br>
-                                <span id="water">90.7 %</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div style="margin-top:50px">
-                        <div class="list-mod">
-                            <ul class="list-mod-2">
-                                <li>
-                                    <span>Auto Water </span>
-                                    <button data-="AUTO_WATER" onclick="app.clickMod(this)" id="btn-auto-water" class="btn-mod">ON</button>
-                                </li>
-                            </ul>
-                        </div>
 
-                    </div>
-                </div>
-                <div id="bar-bottom" class="container">
-            </div>`;
+    pagedetail() {
+        this.showPageDetail();
+        this.UpdateMod();
+        this.printname();
+        this.runfunction = "page-detail";
+    }
+    showMainPage() {
+        document.getElementById("wrapper-all").innerHTML = MAIN_PAGE;
+    }
+    showPageDetail() {
+        document.getElementById("wrapper-all").innerHTML = PAGE_DETAIL;
     }
     printname() {
         document.getElementById("user-name").innerHTML = this.user.name;
@@ -213,8 +251,16 @@ class App {
         c.innerHTML = inner;
         parent.appendChild(c);
     }
+    setting() {
+
+    }
+    logout() {
+
+    }
 }
-let app = new App();
+
+
+
 
 function setCookie(cname, cvalue, exdays) {
     const d = new Date();
@@ -239,14 +285,23 @@ function getCookie(cname) {
     return "";
 }
 
+
+let app = new App();
+
 function checkCookie() {
-    let user = getCookie("username");
-    if (user != "") {
-        alert("Welcome again " + user);
+    let token = getCookie("token");
+    if (token != "") {
+        app.ChangeMainPage();
     } else {
-        user = prompt("Please enter your name:", "");
-        if (user != "" && user != null) {
-            setCookie("username", user, 30);
-        }
+        app.showLoginpage();
     }
 }
+checkCookie();
+
+setInterval(function() {
+    if (app.runfunction === "main-page") {
+        app.Networkmainpage();
+    } else if (app.runfunction === "page-detail") {
+        app.Networkpagedetail();
+    }
+}, 1000);
