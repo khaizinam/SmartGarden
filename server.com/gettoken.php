9ee11b0966ddc58db82961d562c6d2c8b7eee3b1
Leonadon <84978080+khaizinam@users.noreply.github.com>
2022-03-27 23:37:13 +0700

    1

--- server.com/gettoken.php
+++ server.com/gettoken.php
@@ -3,8 +3,6 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Credentials: true ");
 header("Access-Control-Allow-Methods:GET, POST");
 header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
- 
-
 $data =array('token' => "0123456789123456",
  'id'=>'1027');
     $json = json_encode($data);
