7c054eaaa8be19db50a4e1e5a64b1e1f5033792d
LeoPkm2-1 <89697898+LeoPkm2-1@users.noreply.github.com>
2022-04-14 15:55:55 +0700

    fix thanh

--- server.com/addnewmicrobit.php
+++ server.com/addnewmicrobit.php
@@ -7,9 +7,24 @@ $microbit_name;
 $microbit_accessToken;
 $temperature_lower;
 $temperature_upper;
-$owner_id;
-$user_token=$_GET['token'];
+$owner_id=$_GET['id'];
 
+
+// kiem tra ten xem ton tai hay chua
+$query="SELECT microbit_id 
+        FROM microbits
+        WHERE microbit_name='$microbit_name'";
+
+$numresult=$db->num($query);
+if($numresult>=1){  // đã tồn tại microbit
+    echo 'fail';
+}
+else{ // microbit chua ton tai
+    $query="
+    
+    "
+
+}
 
 
 ?>
