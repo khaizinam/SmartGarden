a958662ac650138f448c2f2fb595f00f6e5096f2
LeoPkm2-1 <89697898+LeoPkm2-1@users.noreply.github.com>
2022-04-16 20:36:58 +0700

    temp

--- server.com/checklogin.php
+++ server.com/checklogin.php
@@ -7,7 +7,7 @@ $password=$_GET['password'];
 
 
 
-$query="SELECT user_id AS id ,user_token as token,user_name as name
+$query="SELECT user_id AS id,user_name as 'name'
         FROM users
         WHERE user_name='$username'
         AND user_password = '$password'
@@ -17,8 +17,7 @@ $numresult=$db->num($query);
 if($numresult==1){
     $sql=$db->send($query);
     $row=$sql->fetch_assoc();
-    $data=array('token'=>$row["token"],
-                'username'=>$row["name"],
+    $data=array('username'=>$row["name"],
                 'id'=>$row["id"]);
     $json =json_encode($data);
     echo $json;
