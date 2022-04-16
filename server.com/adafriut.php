a2fa26ecdfc70b1d49c3017fad306957a44640b8
Leonadon <84978080+khaizinam@users.noreply.github.com>
2022-04-16 13:34:16 +0700

    a

--- server.com/adafriut.php
+++ server.com/adafriut.php
@@ -1,1 +1,11 @@
-https://io.adafruit.com/api
+<?php
+$url = "https://io.adafruit.com/api/v2/DuyThinh141592/feeds/back-pump";
+header('Access-Control-Allow-Origin: *');
+$response = file_get_contents($url);
+
+?>
+<div id="json"><?php echo $response;?></div>
+<script>
+    var jsondata = document.getElementById("json").innerHTML;
+    console.log(jsondata);
+</script>
--- server.com/conn.php
+++ server.com/conn.php
@@ -44,5 +44,43 @@ class DataBase
         $result = $this->link->query($query);
         return mysqli_num_rows($result);
     }
+    public function update($user_name,$feed_key, $AIO_key,$value){
+        $url = "https://io.adafruit.com/api/v2/$user_name/feeds/$feed_key/data?X-AIO-key=$AIO_key";
+    
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        
+        $headers = array(
+           "Content-Type: application/json"
+        );
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        
+        $data = <<<DATA
+        {
+          "value": $value
+        }
+        DATA;
+        
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+        
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        
+        $resp = curl_exec($curl);
+        curl_close($curl);
+        var_dump($resp);    
+    }
+    public function GETdata($username , $feed_key) 
+    {
+        $url = "https://io.adafruit.com/api/v2/$username/feeds/$feed_key";
+        return file_get_contents($url);
+    }
+    public function get_all_feeds($username) 
+    {
+        $url = "https://io.adafruit.com/api/v2/$username/feeds";
+        return file_get_contents($url);
+    }
 }  
 ?>
