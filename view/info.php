afb45f72a10e3cf323c371aa86ff37f532c85148
Leonadon <84978080+khaizinam@users.noreply.github.com>
2022-04-20 12:26:49 +0700

    a

--- view/info.php
+++ view/info.php
@@ -1,9 +1,27 @@
 <?php 
     include "../model/conn.php";
-    include "../php/header.php";
     $db = new DataBase();
     include "../php/checkCookie.php";
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300;400;700&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="../asset/css/main.css" />
+    <link rel="shortcut icon" href="../favicon.png" type="image/x-icon" />
+    <script src="../asset/js/jquery.min.js"></script>
+    <title>SmartGaden</title>
+</head>
+
+<body>
     <div id="wr-header">
         <div id="header-1" class="container justify-content-between d-flex">
             <img id="logo" src="../icon.png" alt="logo">
@@ -17,7 +35,7 @@
                     Tài Khoản
                     </button>
                     <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                        <li><a class="dropdown-item" href="#">Thông tin</a></li>
+                        <li><a class="dropdown-item" href="dashboard.php">Dashboard</a></li>
                         <li><a class="dropdown-item" href="login.php?logout=true">Đăng xuất</a></li>
                     </ul>
                 </div>
@@ -25,4 +43,46 @@
         </div>
     </div>
     <div id="wr-body" class="container">
+        <a href="dashboard.php" class="link-history">
+            <?php echo $_COOKIE["user-name"]; ?>
+        </a>
+        <span class="link-history">></span>
+        <span class="link-history">Infomation</span>
+        <br>
+        <h1 class="header">Thông tin cá nhân</h1>
+
+        <div class="info_base">
+            <h4>Thông tin cơ bản</h4>
+            <div class="info_base_">
+                <div class="info_key">Tên</div>
+                <div class="info_value">Nguyễn Văn A</div>
+            </div>
+            <div class="line"></div>
+            <div class="info_base_">
+                <div class="info_key">Số microbit</div>
+                <div class="info_value">X</div>
+            </div>
+        </div>
+
+        <div class="info_base">
+            <h4>Thông tin liên hệ</h4>
+            <div class="info_base_">
+                <div class="info_key">Email</div>
+                <div class="info_value">abc@gmail.com</div>
+            </div>
+            <div class="line"></div>
+            <div class="info_base_">
+                <div class="info_key">ID</div>
+                <div class="info_value">1564151265489256</div>
+            </div>
+            <div class="line"></div>
+            <div class="info_base_">
+                <div class="info_key">Mật khẩu</div>
+                <div class="info_value">**********************</div>
+            </div>
+        </div>
+
     </div>
+</body>
+
+</html>
